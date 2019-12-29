e0cb03e8fbcd8403ecc47c6438950b53ba71a0d6
Sadik Moazzem <sadik-moazzem@outlook.com>
2019-12-29 10:44:45 +0000

    last try

--- api/routes.py
+++ api/routes.py
@@ -32,9 +32,9 @@ def index():
 
 @app.before_request
 def before_request_func():
-    auth_request_token = request.headers.get('x-auth-token')
+    auth_request_token = request.headers.get('Authorization')
     print('token given : ' + str(auth_request_token))
-    AUTH_TOKEN = os.environ.get('AUTH_TOKEN')
+    AUTH_TOKEN = 'Bearer ' + str(os.environ.get('AUTH_TOKEN'))
     if AUTH_TOKEN != auth_request_token:
         response = jsonify({'NOT AUTHORIZED':'Token not valid!'})
         return response, 401
